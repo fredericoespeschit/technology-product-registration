87a26e4b0cdf291a30ec88dfb49a6e047ebea4a7
fredericoespeschit <fespeschitt@gmail.com>
2023-06-19 20:59:14 -0300

    Inicializando o componente product-read

---
+++ frontend/src/app/components/product/product-read/product-read.component.ts
@@ -0,0 +1,21 @@
+import { ProductService } from './../product.service';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../products.model';
+
+@Component({
+  selector: 'app-product-read',
+  templateUrl: './product-read.component.html',
+  styleUrls: ['./product-read.component.css']
+})
+export class ProductReadComponent {
+  products: Product[] = [];
+
+  constructor (private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.read().subscribe(products => {
+      this.products = products;
+      console.log(products)
+    })
+  }
+}
