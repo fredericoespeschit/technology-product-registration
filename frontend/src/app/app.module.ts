59e0e7db2265b90b0a3f81a0695b99686b87d58c
fredericoespeschit <fespeschitt@gmail.com>
2023-06-20 16:57:20 -0300

    Primeiros passos com componente de atualização de produto

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -22,12 +22,12 @@ import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { ProductReadComponent } from './components/product/product-read/product-read.component';
-import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import localePt  from '@angular/common/locales/pt';
 import { registerLocaleData}  from '@angular/common';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 
 registerLocaleData(localePt);
 
@@ -43,7 +43,7 @@ registerLocaleData(localePt);
     HeartFooterDirective,
     ProductCreateComponent,
     ProductReadComponent,
-    ProductRead2Component
+    ProductUpdateComponent
   ],
   imports: [
     BrowserModule,
