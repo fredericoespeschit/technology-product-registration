b1a76677976b7f5163c21cc00d858c64c64defd7
fredericoespeschit <fespeschitt@gmail.com>
2023-06-20 16:38:39 -0300

    Inclusão dos bot~eso de edição e exclusão da linha

--- frontend/src/app/components/product/product-read/product-read.component.ts
+++ frontend/src/app/components/product/product-read/product-read.component.ts
@@ -9,7 +9,7 @@ import { Product } from '../products.model';
 })
 export class ProductReadComponent {
   products: Product[] = [];
-  displayedColumns =  ['id', 'name', 'price'];
+  displayedColumns =  ['id', 'name', 'price', 'action'];
 
   constructor (private productService: ProductService) {}
 
