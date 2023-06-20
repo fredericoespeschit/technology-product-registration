2a04b0ec12844d769738aff148c5d38606ecde8a
fredericoespeschit <fespeschitt@gmail.com>
2023-06-20 16:30:51 -0300

    Tabela de produtos

--- frontend/src/app/components/product/product-read/product-read.component.ts
+++ frontend/src/app/components/product/product-read/product-read.component.ts
@@ -9,13 +9,14 @@ import { Product } from '../products.model';
 })
 export class ProductReadComponent {
   products: Product[] = [];
+  displayedColumns =  ['id', 'name', 'price'];
 
   constructor (private productService: ProductService) {}
 
   ngOnInit(): void {
     this.productService.read().subscribe(products => {
       this.products = products;
-      console.log(products)
+      // console.log(products)
     })
   }
 }
